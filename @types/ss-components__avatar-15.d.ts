722b5da5424fd04df7f2e08399a1815cc05179fa
natnaelTy <natitaye316@gmail.com>
2026-05-04 21:51:39 +0300

    feat: add TypeScript declaration for @ss-components/avatar-15

---
+++ @types/ss-components__avatar-15.d.ts
@@ -0,0 +1,5 @@
+declare module "@ss-components/avatar-15" {
+  import type { ComponentType, ImgHTMLAttributes } from "react";
+  const Avatar15: ComponentType<ImgHTMLAttributes<HTMLImageElement> & { name?: string }>; 
+  export default Avatar15;
+}
