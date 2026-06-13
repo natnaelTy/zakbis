25fd93cdcf6561b882f141cc40450fb22ec09eec
natnaelTy <natitaye316@gmail.com>
2026-06-13 22:58:21 +0300

    feat: implement waitlist landing page components and update UI button and input styles

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -20,11 +20,11 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
+        default: "h-11 px-4 py-3 rounded-sm",
+        sm: "h-9 rounded-sm px-3",
+        lg: "h-11 rounded-sm px-8",
         icon: "h-10 w-10",
-        "icon-xs": "size-6 rounded-md p-0",
+        "icon-xs": "size-6 rounded-sm p-0",
       },
     },
     defaultVariants: {
